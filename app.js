43f42f45515352ee18b3231288953d4d37d85b87
DHK-FPT <nguyendung.software@gmail.com>
2022-04-24 15:20:09 +0700

    View

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
+const shortid = require('shortid');
 const app = express();
 
 const port = 80;
@@ -18,10 +19,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.static('public')); //static files
 
-var users = [
-    {id: 1,name:'hung'},
-    {id: 2,name:'minh'}
-];
+// var users = [
+//     {id: 1,name:'hung'},
+//     {id: 2,name:'minh'}
+// ];
 app.get("/", (req, res) => {
     res.render('index');
 });
@@ -50,10 +51,23 @@ app.get("/users/search", (req, res) =>{
 app.get("/users/create", (req, res) =>{
     res.render("users/create");
 });
+
+app.get('/users/:id', (req,res) => {
+    var id = req.params.id;
+
+    var users = db.get('users').find({id:id}).value();
+    res.render('users/view', {
+        users: users
+    });
+});
+
 app.post("/users/create", (req, res) =>{
+    req.body.id = shortid.generate();
     db.get('users').push(req.body).write();
     res.redirect("/users");
 });
+
+
 
 app.listen(port, () => {
     console.log("Port "+port);
